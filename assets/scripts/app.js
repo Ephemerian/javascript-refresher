db35c425a2bdf43eb4e9d452d38b2687df959970
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2024-02-06 20:42:26 +0200

    solved tasks

---
+++ assets/scripts/app.js
@@ -0,0 +1,70 @@
+// import { apiKey } from "./util.js";
+
+// import apiKey from "./util.js";
+// import { apiKey, abc as content } from "./util.js";
+// import * as util from "./util.js";
+
+// console.log(util.apiKey);
+// console.log(content);
+
+// const userMessage = "Hello World!!!";
+
+// console.log(userMessage);
+// console.log(userMessage);
+
+// function createGreeting(userName, message = "Hello!") {
+// console.log(userName);
+// console.log(message);
+//   return "Hi, I am " + userName + ". " + message;
+// }
+
+// const greeting1 = createGreeting("Max");
+// console.log(greeting1);
+
+// const greeting2 = createGreeting("Manuel", "Hello, what's up?");
+// console.log(greeting2);
+
+// export default (userName, message) => {
+//  console.log("Hello");
+//   return userName + message;
+// };
+
+// const user = {
+//   name: "Max",
+//   age: 34,
+//   greet() {
+//     console.log("Hello!");
+//     console.log(this.age);
+//   }
+// };
+
+// console.log(user.name);
+// user.greet();
+
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   greet() {
+//     console.log("Hi!");
+//   }
+// }
+
+// const user1 = new User("Manuel", 35);
+// console.log(user1);
+// user1.greet();
+
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// console.log(hobbies[0]);
+
+// hobbies.push("Working");
+// console.log(hobbies);
+
+// const index = hobbies.findIndex((item) => item === "Sports");
+
+// console.log(index);
+
+// const editedHobbies = hobbies.map((item) => ({ text: item }));
+// console.log(editedHobbies);
