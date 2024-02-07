6ffa398cca4f4cd97c626e24a37cf637e6aabffc
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2024-02-07 16:36:38 +0200

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -118,16 +118,37 @@
 // }
 
 
-function handleTimeout() {
-    console.log("Timed out!");
-}
+// function handleTimeout() {
+//     console.log("Timed out!");
+// }
 
-const handleTimeout2 = () => {
-    console.log("Timed out.. again!");
-};
+// const handleTimeout2 = () => {
+//     console.log("Timed out.. again!");
+// };
 
-setTimeout(handleTimeout, 2000);
-setTimeout(handleTimeout2, 3000);
-setTimeout(() => {
-    console.log("More timing out...");
-}, 4000);
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//     console.log("More timing out...");
+// }, 4000);
+
+// function greeter(greetFn) {
+//     greetFn();
+// }
+
+// greeter(() => console.log("Hi"));
+
+// function init() {
+//     function greet() {
+//         console.log("Hi!");
+//     }
+
+//     greet();
+// }
+
+// const message = “Hello”;
+
+// const hobbies = ["Sports", "Cooking"];
+// // hobbies = [];
+// hobbies.push("Working");
+// console.log(hobbies);
